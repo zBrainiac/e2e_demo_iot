a68b046975145ce55d43904098dff69811aa9df2
zBrainiac <marcel.daeppen@gmail.com>
2021-01-05 16:19:06 +0100

    0.0.1 - initial release

---
+++ IoT_access_native_JSON.py
@@ -0,0 +1,32 @@
+import time
+
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StringType, IntegerType, DoubleType
+
+spark = SparkSession \
+    .builder \
+    .appName("IoT_access_native_JSON") \
+    .config("spark.yarn.access.hadoopFileSystems", "s3a://demo-aws-2//") \
+    .getOrCreate()
+
+## Get data from geo location data dataset
+print('load data from "geo location" data dataset')
+start_time = time.time()
+
+geolocation_schema = StructType() \
+    .add("sensor_id", IntegerType(), True) \
+    .add("city", StringType(), True) \
+    .add("lat", DoubleType(), True) \
+    .add("lon", DoubleType(), True)
+
+df_geolocation_data_clean = spark.read.format("csv") \
+    .option("header", True) \
+    .schema(geolocation_schema) \
+    .load("s3a://demo-aws-2/user/mdaeppen/data_geo/geolocation_ch.csv")
+
+df_geolocation_data_clean.printSchema()
+
+df_geolocation_data_clean.show(n=5, truncate=False)
+print(df_geolocation_data_clean.count())
+
+print("--- %s 'geo location data cleansing' in seconds ---" % (time.time() - start_time))
